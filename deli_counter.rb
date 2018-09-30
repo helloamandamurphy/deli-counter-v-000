0476e20b434f0e3db8d05c6951868ef04e4fec99
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-30 20:15:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,8 @@ end
 end
 
 def take_a_number(katz_deli,name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{index + 1} in line."
   katz_deli.each_with_index do |name,index|
     if katz_deli.length == 0 
       katz_deli.unshift("#{name}")
